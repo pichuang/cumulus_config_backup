0420a9b6f6bfbd1fcf025cc6721e7497394f8e05
Phil Huang <phil_huang@edge-core.com>
2016-11-08 18:31:45 +0800

    experience import file
    Signed-off-by: Phil Huang <phil_huang@edge-core.com>

---
+++ run_import.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Backup Configuration of CumulusLinux Switch 
+# Author: Phil Huang <phil_huang@edge-core.com>
+#
+
+# Check ansible-playbook is exist, if not return 1
+type ansible-playbook >/dev/null 2>&1 || { echo >&2 "Please install ansible first" ; exit 1; }
+
+# Run ansible-playbook
+ansible-playbook import.yml
+#ansible-playbook import.yml -K
